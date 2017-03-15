40cb451bc5f4d1d6b573f0051242640041d4bfdf
Lena Wayson <lenawayson.dfx@gmail.com>
2017-03-15 15:52:29 -0400

    Add new and create actions

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,14 @@
 class OrganizationController < ApplicationController
   def new
+    @organization = Organization.new
+    end
+
+  def create
+    @organization = Organization.new
+    if @organization.save
+      redirect_to root_path
+    else
+      render new
+    end
   end
 end
