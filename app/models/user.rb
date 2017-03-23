208123a861bb40aec9be94150ccc977a4f0a85cb
Lena Wayson <lenawayson.dfx@gmail.com>
2017-03-23 14:44:04 -0400

    Add organization_id foreign key to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,7 @@
 #  invited_by_type        :string
 #  invited_by_id          :integer
 #  invitations_count      :integer          default("0")
+#  organization_id        :integer
 #
 # Indexes
 #
@@ -36,11 +37,13 @@
 #  index_users_on_invitation_token      (invitation_token) UNIQUE
 #  index_users_on_invitations_count     (invitations_count)
 #  index_users_on_invited_by_id         (invited_by_id)
+#  index_users_on_organization_id       (organization_id)
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 
 class User < ApplicationRecord
   has_one :profile, as: :profileable
+  belongs_to :organization, foreign_key: :organization_id
 
   enum role: [:user, :org_admin, :admin]
   after_initialize :set_default_role, :if => :new_record?
