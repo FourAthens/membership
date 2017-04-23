08bbf828d5950d26013ddaab19193ddcf49f2095
Jordan Burke <me@jordankburke.com>
2017-04-22 20:34:15 -0400

    Added headers to interior pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,15 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
   before_action :load_plans
+  before_action :check_sign_in, only: [:show]
 
   private
+
+  def check_sign_in
+    if params[:id] == 'benefits'
+      authenticate_user!
+    end
+  end
 
   def load_plans
     @monthly_plans = Plan.monthly
