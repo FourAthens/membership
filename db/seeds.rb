a72c6e2cecca14fe5daaea7720148a9e3ccd7656
Jordan Burke <me@jordankburke.com>
2017-05-01 10:05:36 -0400

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
-
 all_details = [
   {content: "Access to O'Reilly Partner discounts", href: "http://www.oreilly.com/partner/"},
   {content: "Free Community Desk Reservations", href: "http://fourathens.skedda.com"},
--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id                 :integer          not null, primary key
+#  cc_exp_month       :string
+#  cc_exp_year        :string
+#  last_four_digits   :string
+#  street_address     :string
+#  city               :string
+#  state              :string
+#  zip_code           :string
+#  stripe_customer_id :string
+#  stripe_token       :string
+#  profileable_type   :string
+#  profileable_id     :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+# Indexes
+#
+#  index_profiles_on_profileable_type_and_profileable_id  (profileable_type,profileable_id)
+#
+
 require 'test_helper'
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
