0d0a40fabe17b8e5540372d811db721979cf2066
PendragonDevelopment <me@jordankburke.com>
2017-04-30 21:55:12 -0400

    Setting deployments

--- Capfile
+++ Capfile
@@ -1,26 +1,18 @@
-# Load DSL and Setup Up Stages
+require 'capistrano/console'
 require 'capistrano/setup'
-
-# Includes default deployment tasks
 require 'capistrano/deploy'
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+require 'capistrano/rbenv'
+require 'capistrano/bundler'
+require 'capistrano/puma'
+require 'capistrano/puma/nginx'
+require 'capistrano/rails/assets'
+require 'capistrano/rails/migrations'
+require 'capistrano/rails/collection'
+require 'capistrano/figaro_yml'
+require 'capistrano/secrets_yml'
+require 'capistrano/faster_assets'
 
-# Includes tasks from other gems included in your Gemfile
-#
-# For documentation on these, see for example:
-#
-#   https://github.com/capistrano/rvm
-#   https://github.com/capistrano/rbenv
-#   https://github.com/capistrano/chruby
-#   https://github.com/capistrano/bundler
-#   https://github.com/capistrano/rails/tree/master/assets
-#   https://github.com/capistrano/rails/tree/master/migrations
-#
-# require 'capistrano/rvm'
-# require 'capistrano/rbenv'
-# require 'capistrano/chruby'
-# require 'capistrano/bundler'
-# require 'capistrano/rails/assets'
-# require 'capistrano/rails/migrations'
-
-# Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
-Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
+# Load custom tasks from `lib/capistrano/tasks` if you have any defined
+Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,65 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+# config valid only for current version of Capistrano
+lock '3.7.2'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :application, 'membership'
+set :repo_url, 'git@github.com:fourathens/membership.git'
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+set :keep_releases, 3
+set :pty, false
 
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
+# RBEnv
+set :rbenv_ruby, '2.3.3'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/application.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :bundle_binstubs, -> { shared_path.join('bin') }
+set :bundle_gemfile, -> { release_path.join('Gemfile') }
+
+# Puma
+set :puma_nginx, :app
+set :puma_init_active_record, true
+set :puma_bind, "unix:///var/www/membership/shared/tmp/sockets/puma.sock"
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :nginx_use_ssl, false
+
+set :migration_role, 'app'            # Defaults to 'db'
+set :conditionally_migrate, true      # Defaults to false. If true, it's skip migration if files in db/migrate not modified
+set :assets_roles, :app
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
 
 namespace :deploy do
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      invoke 'puma:restart'
     end
   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  after :finishing, 'deploy:cleanup'
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
 
 end
