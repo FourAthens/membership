678988ca9ce786238c2cabecfaf309bd06a23c51
PendragonDevelopment <me@jordankburke.com>
2017-06-17 20:13:23 -0400

    Fixed Hubspot service

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -12,13 +12,12 @@ class ProfilesController < ApplicationController
     if @user.save
       @profile = @user.create_profile
       @plan = Plan.find_by(stripe_plan_id: params[:stripe_plan_id])
-      AddContactToHubspot.new(@user.id).call
       sign_in @user
       if @plan.stripe_plan_id.include?("individual")
         response = ProcessStripePayment.new(@user.id, params[:registration][:card_token], params[:stripe_plan_id]).call
         Subscription.create(profile_id: @profile.id, plan_id: @plan.id, status: response.status)
         @profile.update(stripe_customer_id: response.customer, cc_exp_month: params[:user][:profile_attributes][:cc_exp_month], cc_exp_year: params[:user][:profile_attributes][:cc_exp_year], street_address: params[:user][:profile_attributes][:street_address], city: params[:user][:profile_attributes][:city], state: params[:user][:profile_attributes][:state], zip_code: params[:user][:profile_attributes][:zip_code])
-        # AddContactToHubspot.new(@user.id).call
+        AddContactToHubspot.new(@user.id).call
         redirect_to @profile, success: "Congrats! Welcome to Four Athens!"
       else
         redirect_to new_organization_path(token: params[:registration][:card_token], plan_id: @plan.id), success: "Set up your organization account"
