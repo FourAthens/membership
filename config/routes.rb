3318aa855441a3cedabd5c60d0c9b69bb78c0fb7
Jordan Burke <jordan@roundtabledev.com>
2017-07-04 21:18:07 -0400

    SIP Deploy (#21)
    * Fixed styling on org form
    
    * Added SIP page
    
    * Added SIP page
    
    * Updated SIP page and backgrounds

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,6 @@
 
 Rails.application.routes.draw do
 
-  # get "pages/*id" => 'pages#show', as: :page, format: false
   root to: 'visitors#index'
   devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions" }
   resources :users, except: [:edit, :update]
